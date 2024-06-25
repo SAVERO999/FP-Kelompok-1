796ba525d8cb3850ad4bc099b309deb12df6a4e2
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-06-25 15:11:34 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -233,7 +233,7 @@ if selected == "Encyclopedia":
 if selected == "Data Input":
     st.title('Data & Graphic Input')
     st.header("Data Input")
-    st.write(data)
+    st.write(elapsed_time)
 
     # Create the figure with Plotly
     fig = go.Figure(data=go.Scatter(x=elapsed_time, y=y, mode='lines'))
