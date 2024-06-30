b9b0465776c3d2fc9d5af3f8e99eb6d1200617bc
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-06-30 20:13:32 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -195,6 +195,33 @@ w2fm_values = [w2fm[i, :] for i in range(5)]  # Equivalent to w2fm[1,n] to w2fm[
 s2fm_values = [s2fm[i, :] for i in range(5)]  # Equivalent to s2fm[1,n] to s2fm[5,n] in original code (0-based index)
 
 
+w2fb = np.zeros((6, len(ecg) + T5))
+
+
+n_list = list(range(len(ecg)))
+
+# Perform calculations
+for n in n_list:
+    for j in range(1, 6):
+        w2fb[1][n + T1] = 0
+        w2fb[2][n + T2] = 0
+        w2fb[3][n + T3] = 0
+        a = -(round(2**j) + round(2**(j - 1)) - 2)
+        b = -(1 - round(2**(j - 1)))
+        for k in range(a, b + 1):
+            index = n - (k + abs(a))
+            if 0 <= index < len(ecg):
+                w2fb[1][n + T1] += qj[1][k + abs(a)] * ecg[index]
+                w2fb[2][n + T2] += qj[2][k + abs(a)] * ecg[index]
+                w2fb[3][n + T3] += qj[3][k + abs(a)] * ecg[index]
+                w2fb[4][n + T3] += qj[4][k + abs(a)] * ecg[index]
+                w2fb[5][n + T3] += qj[5][k + abs(a)] * ecg[index]
+
+# Create and display plots for each DWT level
+figs = []
+n = np.arange(1000)
+
+
 
 
 
@@ -447,7 +474,27 @@ if selected == "DWT":
             for i in range(5):
                 create_plot(n_values, s2fm_values[i], i, 's2fm')
    if sub_selected  == 'Filter Bank':
-            st.title("t")
+           # Initialize a list to store figures
+            figs = []
+            
+            # Create and append figures to the list
+            for i in range(1, 6):
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(x=n, y=w2fb[i][:len(n)], mode='lines', name=f'Orde {i}'))
+                fig.update_layout(
+                    title=f'Plot Orde {i}',
+                    xaxis_title='elapsed_time',
+                    yaxis_title='Nilai',
+                    template='plotly_dark',
+                    height=400,
+                    width=1500,
+                )
+                figs.append(fig)
+            
+            # Display each figure using Streamlit
+            for i, fig in enumerate(figs):
+                st.header(f'Plot Orde {i+1}')
+                st.plotly_chart(fig)
         
 
     
