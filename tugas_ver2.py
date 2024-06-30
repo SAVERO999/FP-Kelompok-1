aaf69a1322e5b7bb395e592a48f9e9b361e320e5
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-06-30 19:20:44 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -79,14 +79,7 @@ for i in range(0, round(fs/2) + 1):
 
 traces = []
 
-qj = np.zeros ((6, 10000))
-k_list = []
-j= 1
-a = -(round (2**j) + round (2**(j-1)) - 2 )
-b=-(1- round(2**(j-1)))+1
-for k in range (a,b):
-  k_list.append(k)
-  qj[1][k+abs(a)] = -2 * ( dirac(k) - dirac(k+1) )
+
 
 
 k_list = []
@@ -233,8 +226,24 @@ if selected == "DWT":
             fig = go.Figure(data=traces, layout=layout)
             st.plotly_chart(fig)
      if selected_optimizer == 'q1(k)':
+            qj = np.zeros((6, 10000))
+            k_list = []
+            j = 1
+            
+            # Calculations
+            a = -(round(2**j) + round(2**(j-1)) - 2)
+            st.write(f"a = {a}")
+            b = -(1 - round(2**(j-1))) + 1
+            st.write(f"b = {b}")
+            
+            for k in range(a, b):
+                k_list.append(k)
+                qj[1][k + abs(a)] = -2 * (dirac(k) - dirac(k+1))
+            
+            # Visualization using Plotly
             fig = go.Figure(data=[go.Bar(x=k_list, y=qj[1][0:len(k_list)])])
             fig.update_layout(title='', xaxis_title='', yaxis_title='')
+            
             st.plotly_chart(fig)
      if selected_optimizer == 'q2(k)':
          fig = go.Figure(data=[go.Bar(x=k_list, y=qj[2][0:len(k_list)])])
