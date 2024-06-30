cd22b4fe590bcdb52f56a29e6645783a00b5ce89
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-06-30 19:04:33 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -173,8 +173,6 @@ if selected == "DWT":
             fig = go.Figure(data=traces, layout=layout)
             st.plotly_chart(fig)
 
-
-
      if selected_optimizer == 'q1(k)':
      if selected_optimizer == 'q2(k)':
      if selected_optimizer == 'q3(k)':
